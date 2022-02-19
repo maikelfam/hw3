0ed786f76b150b4f61f8bc857a45f9d534f64d86
maikelfam <maikel.fam@kellogg.northwestern.edu>
2022-02-19 19:31:57 +0000

    set up controllers

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,18 @@
+class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all 
+    end 
+
+    def show
+    
+    end
+
+    def new
+        @place = Place.new
+    end 
+
+    def create
+
+    end 
+end
