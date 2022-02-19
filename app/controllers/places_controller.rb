4d5fc9332fa06b53b4ce01586e98f1535a429a28
maikelfam <maikel.fam@kellogg.northwestern.edu>
2022-02-19 22:11:18 +0000

    error in opening created place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,7 @@ class PlacesController < ApplicationController
     end 
 
     def show
-    
+        @place = Place.find(params["id"])
     end
 
     def new
@@ -13,6 +13,8 @@ class PlacesController < ApplicationController
     end 
 
     def create
-
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
     end 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
 
-    def index
-        @posts = Post.all
-    end 
-
-    def new
+     def new
         @post = Post.new
     end 
 
     def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places"
     end 
     
+    def show
+        @post = Post.find(params["id"])
+    end 
 end
